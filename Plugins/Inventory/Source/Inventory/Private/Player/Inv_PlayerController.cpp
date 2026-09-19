fb28e20bd5e711b416cc015dbd4e9efa4ba818cd
1Xizt <907870229@qq.com>
2026-09-19 16:49:57 +0800

    Add C++ source and configuration

---
+++ Plugins/Inventory/Source/Inventory/Private/Player/Inv_PlayerController.cpp
@@ -0,0 +1,91 @@
+﻿#include "Player/Inv_PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Kismet/GameplayStatics.h"
+#include "Widgets/HUD/HUDWidget.h"
+
+AInv_PlayerController::AInv_PlayerController()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	
+	TraceLength = 500.f;
+}
+
+void AInv_PlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	
+	TraceForItem();
+}
+
+void AInv_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (IsValid(Subsystem))
+	{
+		Subsystem->AddMappingContext(DefaultIMC, 0);
+	}
+	
+	CreateHUDWidget();
+}
+
+void AInv_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(PrimaryInteractAction, ETriggerEvent::Started, this, &AInv_PlayerController::PrimaryInteract);
+}
+
+void AInv_PlayerController::PrimaryInteract()
+{
+	UE_LOG(LogTemp, Log, TEXT("Primary Interact"))
+}
+
+void AInv_PlayerController::CreateHUDWidget()
+{
+	ensure(IsValid(HUDWidgetClass));
+
+	if (!IsLocalController()) return;
+ 	
+	HUDWidget = CreateWidget<UHUDWidget>(this, HUDWidgetClass);
+	if (IsValid(HUDWidget))
+	{
+		HUDWidget->AddToViewport();
+	}
+}
+
+void AInv_PlayerController::TraceForItem()
+{
+	if (!IsValid(GEngine) || !IsValid(GEngine->GameViewport)) return;
+	FVector2D ViewportSize;
+	GEngine->GameViewport->GetViewportSize(ViewportSize);
+	const FVector2D ViewportCenter = ViewportSize / 2.f;
+	FVector TraceStart;
+	FVector Forward;
+	if (!UGameplayStatics::DeprojectScreenToWorld(this, ViewportCenter, TraceStart, Forward)) return;
+
+	const FVector TraceEnd = TraceStart + Forward * TraceLength;
+	FHitResult HitResult;
+	GetWorld()->LineTraceSingleByChannel(HitResult, TraceStart, TraceEnd, ItemTraceChannel);
+
+	LastActor = ThisActor;
+	ThisActor = HitResult.GetActor();
+
+	if (ThisActor == LastActor) return;
+
+	if (ThisActor.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Started tracing a new actor."))
+	}
+
+	if (LastActor.IsValid())
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Stopped tracing last actor."))
+	}
+}
+	
---
+++ Plugins/Inventory/Source/Inventory/Public/Player/Inv_PlayerController.h
@@ -0,0 +1,55 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Inv_PlayerController.generated.h"
+
+class UHUDWidget;
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class INVENTORY_API AInv_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+public:
+	AInv_PlayerController();
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+
+	void PrimaryInteract();
+	
+	void CreateHUDWidget();
+	
+	void TraceForItem();
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputMappingContext> DefaultIMC;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TObjectPtr<UInputAction> PrimaryInteractAction;
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TSubclassOf<UHUDWidget> HUDWidgetClass;
+	
+	UPROPERTY()
+	TObjectPtr<UHUDWidget> HUDWidget;
+	
+	
+	
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	double TraceLength;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Inventory")
+	TEnumAsByte<ECollisionChannel> ItemTraceChannel;
+
+	TWeakObjectPtr<AActor> ThisActor;
+	TWeakObjectPtr<AActor> LastActor;
+};
